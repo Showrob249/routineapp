580d63e518c0844b2c34d83c73ebac2759888004
Showrob249 <dreamboyking50@gmail.com>
2026-02-20 18:05:10 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,7 @@
-const CACHE_NAME = "routine-cache-v1";
-
-const urlsToCache = [
-  "/routineapp/",
-  "/routineapp/index.html",
-  "/routineapp/manifest.json",
-  "/routineapp/icon.png"
-];
-
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-// 🔔 Push Event (PART 3)
-self.addEventListener("push", event => {
-  const data = event.data.json();
-
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    icon: "/routineapp/icon.png",
-    badge: "/routineapp/icon.png"
-  });
+self.addEventListener("activate", event => {
+  console.log("Service Worker Activated");
 });
