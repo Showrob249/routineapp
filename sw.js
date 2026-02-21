4b0d86d14d5035c7a3c44777b5a9d9d3931bcc8d
Showrob249 <dreamboyking50@gmail.com>
2026-02-21 22:59:05 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
-self.addEventListener("install", event => {
-  console.log("Service Worker Installed");
+const CACHE_NAME="routine-app-v1";
+const urlsToCache=["./","./index.html"];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache=>cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch",event=>{
+  event.respondWith(
+    caches.match(event.request)
+    .then(response=> response || fetch(event.request))
+  );
 });
